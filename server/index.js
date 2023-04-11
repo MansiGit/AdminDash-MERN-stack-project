0d6f6bb8cae3d2230380cb9b4a525a0d9a593bfc
MansiGit <mrborole@gmail.com>
2023-04-10 21:57:23 -0400

    Added admin, breakdown, daily, monthly,performance pages

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 import {
     dataUser,
@@ -57,7 +58,8 @@ mongoose.connect(process.env.MONGO_URL, {
     //ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
     //OverallStat.insertMany(dataOverallStat);
-    
+    //AffiliateStat.insertMany(dataAffiliateStat);
+
 
 }).catch((error) => console.log(`${error} did not connect`));
 
